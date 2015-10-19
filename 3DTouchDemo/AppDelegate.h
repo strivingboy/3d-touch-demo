8b34e191d708be6407912ccbbcc496fc95895b91
tusu <striving_boy@163.com>
2015-10-19 19:21:09 +0800

    Initial Commit

---
+++ 3DTouchDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  3DTouchDemo
+//
+//  Created by tusu on 15/10/19.
+//  Copyright © 2015年 strivingboy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 3DTouchDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  3DTouchDemo
+//
+//  Created by tusu on 15/10/19.
+//  Copyright © 2015年 strivingboy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
